9f6966c4db38509715c18ddb9be1f8351dc1ff49
Evinsson Medrano <evinssonmedrano@gmail.com>
2025-03-04 16:29:33 -0600

    Add eslint+prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactX from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist/', 'node_modules/'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.app.json'],
+      },
     },
     plugins: {
+      '@typescript-eslint': tseslint,
+      react: react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-x': reactX,
+      'react-dom': reactDom,
+      prettier: eslintPluginPrettier,
     },
     rules: {
+      ...tseslint.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...reactX.configs['recommended-typescript'].rules,
+      ...reactDom.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+
+      'prettier/prettier': 'error',
     },
   },
-)
+];
