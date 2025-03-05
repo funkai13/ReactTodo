e410bbfa800f3c1057f5c034e7f25c37cbde4109
evinsson medrano <evinssonmedrano@gmail.com>
2025-03-05 17:35:29 -0600

    Update auth.service.ts add email to  storage

--- src/features/auth/services/auth.service.ts
+++ src/features/auth/services/auth.service.ts
@@ -12,6 +12,7 @@ export const loginService = async (
   try {
     const response = await apiClient.post<LoginResponse>('login', credentials);
     useAuthStore.getState().setToken(response.data.data.token);
+    useAuthStore.getState().setEmail(credentials.email);
     const result = response.data.data;
     console.log(result);
 
