3d0efdf376721c0281d6a0657048b3bdd288b10b
evinsson medrano <evinssonmedrano@gmail.com>
2025-03-05 17:39:22 -0600

    Update edit-tastk.store.ts

--- src/features/task/store/edit-tastk.store.ts
+++ src/features/task/store/edit-tastk.store.ts
@@ -1,6 +1,5 @@
+// edit-task.store.ts
 import { create } from 'zustand';
-
-import { taskService } from '@/features/task/services/task.service.ts';
 import { Task } from '@/features/task/interfaces/Task.interface.ts';
 
 interface EditTaskStore {
@@ -8,8 +7,6 @@ interface EditTaskStore {
   selectedTask: Task | null;
   openModal: (task: Task) => void;
   closeModal: () => void;
-  handleSave: (updatedTask: Task) => Promise<void>;
-  handleDelete: (taskId: number) => Promise<void>;
 }
 
 export const useEditTaskStore = create<EditTaskStore>((set) => ({
@@ -18,20 +15,4 @@ export const useEditTaskStore = create<EditTaskStore>((set) => ({
 
   openModal: (task) => set({ isOpen: true, selectedTask: task }),
   closeModal: () => set({ isOpen: false, selectedTask: null }),
-
-  handleSave: async (updatedTask) => {
-    try {
-      await taskService.updateTask(updatedTask.id, updatedTask);
-    } catch (error) {
-      throw error;
-    }
-  },
-
-  handleDelete: async (taskId) => {
-    try {
-      await taskService.deleteTask(taskId);
-    } catch (error) {
-      throw error;
-    }
-  },
 }));
