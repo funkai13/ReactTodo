10d52a14f991d8bb58fbdf63e2d3fa08ac1e39ad
evinsson medrano <evinssonmedrano@gmail.com>
2025-03-04 23:05:34 -0600

    Delete loginApi.ts

--- src/features/auth/lib/loginApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const loginApi = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
-});
