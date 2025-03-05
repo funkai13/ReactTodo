b1283c64aeaa0ab844055be5060d084e7a1d1aa6
evinsson medrano <evinssonmedrano@gmail.com>
2025-03-04 23:05:26 -0600

    Add shadcnUi components

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useState } from 'react';
+import { useNavigate } from 'react-router';
 import { loginService } from '@/features/auth/services/auth.service.ts';
 import {
   Card,
@@ -12,12 +13,15 @@ import {
 import { Button } from '@/components/ui/button.tsx';
 import { toast } from 'sonner';
 import { useAuthStore } from '@/features/auth/store/auth.store.ts';
+import { Input } from '@/components/ui/input.tsx';
+import { Label } from '@radix-ui/react-label';
 
 export const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const { setToken } = useAuthStore();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -30,6 +34,7 @@ export const LoginForm = () => {
       const login = await loginService(loginDataRequest);
       setToken(login.token);
       toast.success('Inicio de Sesion Exitoso');
+      navigate('/task');
     } catch (e) {
       setError(`Error al iniciar sesion ${e}`);
       toast.error(error);
@@ -45,9 +50,9 @@ export const LoginForm = () => {
       </CardHeader>
       <form onSubmit={handleSubmit}>
         <CardContent className="space-y-4">
-          <div className="space-y-2">
-            <label htmlFor="email">Email</label>
-            <input
+          <div className="space-y-6 mb-6">
+            <Label htmlFor="email">Email</Label>
+            <Input
               id="email"
               type="email"
               placeholder="tu@email.com"
