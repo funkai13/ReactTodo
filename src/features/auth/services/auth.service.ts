8ac037805cc4aec0373d80de350170627b653e9d
evinsson medrano <evinssonmedrano@gmail.com>
2025-03-04 23:04:53 -0600

    Update auth.service.ts

--- src/features/auth/services/auth.service.ts
+++ src/features/auth/services/auth.service.ts
@@ -1,17 +1,17 @@
-import { useAuthStore } from '@/features/auth/store/auth.store.ts';
 import {
-  ILogin,
-  ILoginRespose,
+  Login,
   LoginDataRequest,
+  LoginResponse,
 } from '@/features/auth/interfaces/LoginDataRequest.interface.ts';
-import { loginApi } from '@/features/auth/lib/loginApi.ts';
+import { apiClient } from '@/lib/apiClient.ts';
+import { useAuthStore } from '@/features/auth/store/auth.store.ts';
 
 export const loginService = async (
   credentials: LoginDataRequest
-): Promise<ILogin> => {
+): Promise<Login> => {
   try {
-    const response = await loginApi.post<ILoginRespose>('/login', credentials);
-
+    const response = await apiClient.post<LoginResponse>('login', credentials);
+    useAuthStore.getState().setToken(response.data.data.token);
     const result = response.data.data;
     console.log(result);
 
@@ -23,7 +23,13 @@ export const loginService = async (
 };
 
 export const authService = {
+  login: async (credentials: { email: string; password: string }) => {
+    const response = await apiClient.post('/auth/login', credentials);
+    useAuthStore.getState().setToken(response.data.token); // Guarda el token en Zustand
+    return response.data;
+  },
   logout: () => {
     useAuthStore.getState().setToken(null);
+    apiClient.defaults.headers.common.Authorization = ''; // Limpia el header
   },
 };
